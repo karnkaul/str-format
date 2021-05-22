123b3a8d5d956c1291282aec8e5dc8d102d94f9b
Karn Kaul <karnkaul@gmail.com>
2021-05-22 10:55:43 -0700

    Add documentation, change forwarding to const refs, make impl more succinct

--- str_format.hpp
+++ str_format.hpp
@@ -7,27 +7,33 @@
 #include <string_view>
 
 namespace kt {
-inline constexpr std::string_view fmt_token = "{}";
+///
+/// \brief Format token
+///
+constexpr std::string_view fmt_token = "{}";
 
-inline std::ostream& format_str(std::ostream& out, std::string_view fmt) {
-	out << fmt;
-	return out;
-}
+///
+/// \brief Obtain formatted string
+/// \param fmt Interpolated string format (only exact fmt_token literal supported for direct argument replacement)
+/// \param args Arguments
+///
+template <typename... Args>
+std::string format_str(std::string_view fmt, Args const&... args);
+
+// impl
+
+inline std::ostream& format_str(std::ostream& out, std::string_view fmt) { return out << fmt; }
 
 template <typename Arg, typename... Args>
-std::ostream& format_str(std::ostream& out, std::string_view fmt, Arg&& arg, Args&&... args) {
-	if (auto search = fmt.find(fmt_token); search != std::string::npos) {
-		std::string_view text(fmt.data(), search);
-		out << text << std::forward<Arg>(arg);
-		return format_str(out, fmt.substr(search + fmt_token.size()), std::forward<Args>(args)...);
-	}
+std::ostream& format_str(std::ostream& out, std::string_view fmt, Arg const& arg, Args const&... args) {
+	if (auto i = fmt.find(fmt_token); i != std::string::npos) { return format_str(out << fmt.substr(0, i) << arg, fmt.substr(i + fmt_token.size()), args...); }
 	return format_str(out, fmt);
 }
 
 template <typename... Args>
-std::string format_str(std::string_view fmt, Args&&... args) {
+std::string format_str(std::string_view fmt, Args const&... args) {
 	std::stringstream str;
-	format_str(str, fmt, std::forward<Args>(args)...);
+	format_str(str, fmt, args...);
 	return str.str();
 }
 } // namespace kt
